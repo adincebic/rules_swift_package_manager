54b12ee1ec2018a0c271045765b54b1ea0b6e9af
Adin Cebic <cebic.ad@gmail.com>
2025-05-04 12:32:03 +0200

    Reproduce KSCrash linker errors

--- examples/resources_example/swift/Package.swift
+++ examples/resources_example/swift/Package.swift
@@ -12,6 +12,8 @@ let package = Package(
         .package(url: "https://github.com/Iterable/swift-sdk", from: "6.5.11"),
         .package(url: "https://github.com/SDWebImage/SDWebImageSwiftUI.git", from: "3.1.3"),
         .package(url: "https://github.com/google/GoogleSignIn-iOS", from: "8.0.0"),
+        //.package(url: "https://github.com/embrace-io/embrace-apple-sdk", exact: "6.9.2"),
+        .package(url: "https://github.com/kstenerud/KSCrash", exact: "2.1.0"),
         .package(
             url: "https://github.com/GoogleCloudPlatform/recaptcha-enterprise-mobile-sdk",
             from: "18.7.0"
